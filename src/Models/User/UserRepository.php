8504a39835aad0f36da9c46baa4b59decfa69ece
Maiko <maikosa@gmx.de>
2025-04-24 09:38:44 +0200

    Fix minor issues
    - Use Table constant in UserRepository
    - Make route lowercase or else it cannot be accessed
    - Add logging if user was not found
    - Change Response from previous array style to the new object style

--- src/Models/User/UserRepository.php
+++ src/Models/User/UserRepository.php
@@ -33,8 +33,9 @@ class UserRepository extends Repository
 		if (!preg_match("/^[a-zA-Z0-9]+$/", $userId)) {
 			throw new \InvalidArgumentException("User ID contains invalid characters");
 		}
+		$table = self::TABLENAME_USER;
 		$query = <<<SQL
-			SELECT * FROM users WHERE id = ?
+			SELECT * FROM {$table} WHERE id = ?
 		SQL;
 		$stmt = $this->databaseConnection->prepare($query);
 		$stmt->execute([$userId]);
