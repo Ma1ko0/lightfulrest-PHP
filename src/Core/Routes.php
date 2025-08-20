94744c0ca949c64ceedb013be0f3013c01537f10
Maiko <maikosa@gmx.de>
2025-08-20 08:52:18 +0200

    Add CORS preflight route (#27)

--- src/Core/Routes.php
+++ src/Core/Routes.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Response;
 use App\Router;
 use App\UserController;
 
 $router = new Router();
 $router->add(Methods::GET, '/users/(\d+)', [UserController::class, 'getUserDataByID']);
+
+
+// CORS Route
+$router->add(Methods::OPTIONS, '/(.*)', function () {
+	Response::empty();
+});
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $router->dispatch($_SERVER["REQUEST_METHOD"], $uri);
